ec4a2c91ea938dcddfc9ac91c33a1f8148170d39
Corentin Aulagnet <corentin.aulagnet@gmail.com>
2026-03-30 21:49:48 +0200

    Fixes the navbar for mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -24,14 +24,11 @@ export default function Example() {
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
               <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
               <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-              
             </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
@@ -48,4 +45,24 @@ export default function Example() {
                 ))}
               </div>
             </div>
-          </div></div></div></Disclosure>);}
+          </div>
+          </div>
+          </div>
+           <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                      currentRoute === item.href ? "text-white bg-gray-900" : "text-gray-300 hover:bg-white/5 hover:text-white"
+    ,                   "rounded-md px-3 py-2 text-sm font-medium")}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+          </Disclosure>);}
