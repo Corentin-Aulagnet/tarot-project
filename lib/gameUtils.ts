ca941f8ddffe60aea5a3b6c88636e3adfbceaccd
Corentin Aulagnet <corentin.aulagnet@gmail.com>
2026-05-20 15:03:30 +0200

    use to break if less than 10 games were saved

--- lib/gameUtils.ts
+++ lib/gameUtils.ts
@@ -4,7 +4,7 @@ export function getLastPlayers(games: Games[], players: Players[]) {
   let playerIds: string[] = [];
   for (let i = 0; i < 10; i++) {
     const gameIndex = i;
-  if (gameIndex < 0) break;
+  if (gameIndex < 0 || gameIndex >= games.length) break;
   const game = games[gameIndex];
   playerIds = [...playerIds].concat(game.players_uid);
   }
